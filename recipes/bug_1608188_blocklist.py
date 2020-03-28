ffc0a4ae7702f0e43632921c81833c7d8ab2ff26
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-03-28 11:01:26 +0100

    Add whitespaces trim

--- recipes/bug_1608188_blocklist.py
+++ recipes/bug_1608188_blocklist.py
@@ -17,18 +17,18 @@ def migrate(ctx):
     transforms_from(
 """
 blocklist-window =
-    .title = { COPY(from_path, "blocklist.title") }
-    .style = { COPY(from_path, "blocklist.style") }
+    .title = { COPY(from_path, "blocklist.title", trim:"True") }
+    .style = { COPY(from_path, "blocklist.style", trim:"True") }
 
-blocklist-soft-and-hard = { COPY(from_path, "blocklist.softandhard") }
-blocklist-hard-blocked = { COPY(from_path, "blocklist.hardblocked") }
-blocklist-soft-blocked = { COPY(from_path, "blocklist.softblocked") }
+blocklist-soft-and-hard = { COPY(from_path, "blocklist.softandhard", trim:"True") }
+blocklist-hard-blocked = { COPY(from_path, "blocklist.hardblocked", trim:"True") }
+blocklist-soft-blocked = { COPY(from_path, "blocklist.softblocked", trim:"True") }
 blocklist-more-information =
-    .value = { COPY(from_path, "blocklist.moreinfo") }
+    .value = { COPY(from_path, "blocklist.moreinfo", trim:"True") }
 blocklist-blocked =
-    .label = { COPY(from_path, "blocklist.blocked.label") }
+    .label = { COPY(from_path, "blocklist.blocked.label", trim:"True") }
 blocklist-checkbox =
-    .label = { COPY(from_path, "blocklist.checkbox.label")}
+    .label = { COPY(from_path, "blocklist.checkbox.label", trim:"True") }
 """,from_path="toolkit/chrome/mozapps/extensions/blocklist.dtd"))
 
     ctx.add_transforms(
@@ -46,13 +46,15 @@ blocklist-checkbox =
                             {
                                 "&brandShortName;": TERM_REFERENCE("brand-short-name")
                             },
+                            trim=True,
                         )
                     ),
                     FTL.Attribute(
                         id=FTL.Identifier("accesskey"),
                         value=COPY(
                             "toolkit/chrome/mozapps/extensions/blocklist.dtd",
-                            "blocklist.accept.accesskey"
+                            "blocklist.accept.accesskey",
+                            trim=True,
                         )
                     ),
                 ]
@@ -65,6 +67,7 @@ blocklist-checkbox =
                     {
                         "&brandShortName;": TERM_REFERENCE("brand-short-name")
                     },
+                    trim=True,
                 )
             ),
 
