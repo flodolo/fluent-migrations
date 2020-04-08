a7d773a90a2d627ff930bdb66272cb321942a246
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-04-08 08:45:37 +0200

    Add whitespaces trim

--- recipes/bug_1625480_appMenuNotifications.py
+++ recipes/bug_1625480_appMenuNotifications.py
@@ -16,21 +16,21 @@ def migrate(ctx):
     transforms_from(
 """
 appmenu-new-tab-controlled =
-    .label = { COPY(from_path, "newTabControlled.header.message") }
-    .buttonlabel = { COPY(from_path, "newTabControlled.keepButton.label") }
-    .buttonaccesskey = { COPY(from_path, "newTabControlled.keepButton.accesskey") }
-    .secondarybuttonlabel = { COPY(from_path, "newTabControlled.disableButton.label") }
-    .secondarybuttonaccesskey = { COPY(from_path, "newTabControlled.disableButton.accesskey") }
+    .label = { COPY(from_path, "newTabControlled.header.message", trim:"True") }
+    .buttonlabel = { COPY(from_path, "newTabControlled.keepButton.label", trim:"True") }
+    .buttonaccesskey = { COPY(from_path, "newTabControlled.keepButton.accesskey", trim:"True") }
+    .secondarybuttonlabel = { COPY(from_path, "newTabControlled.disableButton.label", trim:"True") }
+    .secondarybuttonaccesskey = { COPY(from_path, "newTabControlled.disableButton.accesskey", trim:"True") }
 appmenu-homepage-controlled =
-    .label = { COPY(from_path, "homepageControlled.header.message") }
-    .buttonlabel = { COPY(from_path, "homepageControlled.keepButton.label") }
-    .buttonaccesskey = { COPY(from_path, "homepageControlled.keepButton.accesskey") }
-    .secondarybuttonlabel = { COPY(from_path, "homepageControlled.disableButton.label") }
-    .secondarybuttonaccesskey = { COPY(from_path, "homepageControlled.disableButton.accesskey") }
+    .label = { COPY(from_path, "homepageControlled.header.message", trim:"True") }
+    .buttonlabel = { COPY(from_path, "homepageControlled.keepButton.label", trim:"True") }
+    .buttonaccesskey = { COPY(from_path, "homepageControlled.keepButton.accesskey", trim:"True") }
+    .secondarybuttonlabel = { COPY(from_path, "homepageControlled.disableButton.label", trim:"True") }
+    .secondarybuttonaccesskey = { COPY(from_path, "homepageControlled.disableButton.accesskey", trim:"True") }
 appmenu-tab-hide-controlled =
-    .label = { COPY(from_path, "tabHideControlled.header.message") }
-    .buttonlabel = { COPY(from_path, "tabHideControlled.keepButton.label") }
-    .buttonaccesskey = { COPY(from_path, "tabHideControlled.keepButton.accesskey") }
-    .secondarybuttonlabel = { COPY(from_path, "tabHideControlled.disableButton.label") }
-    .secondarybuttonaccesskey = { COPY(from_path, "tabHideControlled.disableButton.accesskey") }
+    .label = { COPY(from_path, "tabHideControlled.header.message", trim:"True") }
+    .buttonlabel = { COPY(from_path, "tabHideControlled.keepButton.label", trim:"True") }
+    .buttonaccesskey = { COPY(from_path, "tabHideControlled.keepButton.accesskey", trim:"True") }
+    .secondarybuttonlabel = { COPY(from_path, "tabHideControlled.disableButton.label", trim:"True") }
+    .secondarybuttonaccesskey = { COPY(from_path, "tabHideControlled.disableButton.accesskey", trim:"True") }
 """, from_path="browser/chrome/browser/browser.dtd"))
