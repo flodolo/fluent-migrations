5ea14cd9b42078d67d37e7a2575c6970b9b3cd49
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-04-28 08:47:08 +0200

    Add bug template to clean up script

--- scripts/cleanup_migrations.py
+++ scripts/cleanup_migrations.py
@@ -12,6 +12,7 @@ import sys
 
 
 def main():
+    bug_template = 'https://bugzilla.mozilla.org/enter_bug.cgi?assigned_to=nobody%40mozilla.org&bug_ignored=0&bug_severity=normal&bug_status=NEW&bug_type=task&cc=francesco.lodolo%40gmail.com&cc=l10n%40mozilla.com&cf_fx_iteration=---&cf_fx_points=---&comment=Remove%20migration%20recipes%20for%20Firefox%20{version}.%0D%0A%0D%0A{bugs}&component=Fluent%20Migration&contenttypemethod=list&contenttypeselection=text%2Fplain&defined_groups=1&filed_via=standard_form&flag_type-4=X&flag_type-607=X&flag_type-800=X&flag_type-803=X&flag_type-936=X&form_name=enter_bug&maketemplate=Remember%20values%20as%20bookmarkable%20template&op_sys=Unspecified&priority=--&product=Localization%20Infrastructure%20and%20Tools&rep_platform=Unspecified&short_desc=Remove%20Fluent%20migration%20recipes%20for%20Firefox%20{version}&target_milestone=---&version=unspecified'
 
     # Get absolute path of the repository's root from the script location
     root_folder = os.path.abspath(os.path.join(
@@ -41,7 +42,8 @@ def main():
 
     # Get the list of recipes
     recipes_path = os.path.join(root_folder, 'recipes', 'fx{}')
-    recipes_path = recipes_path.format(args.version_number)
+    version_number = args.version_number
+    recipes_path = recipes_path.format(version_number)
     hg_path = os.path.join(
         mozilla_unified_path, 'python', 'l10n', 'fluent_migrations')
 
@@ -50,11 +52,20 @@ def main():
         recipes = [f for f in files if not f[0] == '.']
     try:
         recipes.sort()
-        print('Affected bugs:')
+        output = []
+        output.append('Affected bugs:')
         for recipe in recipes:
-            print('- Bug {}'.format(recipe.split('_')[1]))
+            output.append('- Bug {}'.format(recipe.split('_')[1]))
             # Remove the files from mozilla-unified
             os.remove(os.path.join(hg_path, recipe))
+        print('\n'.join(output))
+        # Print link to bug template
+        encoded_output = [l.replace(' ', '%20') for l in output]
+        print('\nBug template:\n')
+        print(bug_template.format(
+            version=args.version_number,
+            bugs='%0D%0A'.join(encoded_output)
+        ))
     except NameError:
         print('No recipes found')
 
