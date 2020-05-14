bd4ce43cdcad25641f5a0845798fdad96769bfba
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-05-14 18:28:28 +0200

    Add CLI arguments to script for obsolete strings

--- scripts/clean_obsolete_strings.py
+++ scripts/clean_obsolete_strings.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 '''
-This script is used to remove obsolete strings from all locales,
-and reformat files accordingly to the source content (en-US).
+
+This script is used to remove obsolete strings and reformat files accordingly
+to the source content (en-US).
+
+By default, the script runs on all locales, pulls from the repository but
+doesn't commit local changes. To commit:
+
+./clean_obsolete_strings.py --wetrun
+
+See "./clean_obsolete_strings.py --help" for other options.
+
 '''
 
 from compare_locales.parser import getParser
 from compare_locales.serializer import serialize
+import argparse
 import local_config
 import os
 import subprocess
@@ -55,6 +65,27 @@ def extractFileList(repository_path):
 
 
 def main():
+    p = argparse.ArgumentParser(
+        description='Remove obsolete strings and reformat files in localized repositories')
+
+    p.add_argument(
+        '--noupdates',
+        help='Do not pull from remote',
+        action='store_true'
+    )
+    p.add_argument(
+        '--wetrun',
+        help='Commit local changes and push them to remote',
+        action='store_true'
+    )
+    p.add_argument(
+        '--locale',
+        help='Run on a specific locale',
+        action='store',
+        default=''
+    )
+    args = p.parse_args()
+
     # Read paths from config file
     try:
         [l10n_clones_path, quarantine_path] = local_config.read_config(
@@ -63,26 +94,31 @@ def main():
         print('Error reading paths from config')
         print(e)
 
-    locales = [x for x in os.listdir(
-        l10n_clones_path) if not x.startswith('.')]
-    # Exclude locales still working on Mercurial directly
-    excluded_locales = [
-        'da', 'it', 'ja', 'ja-JP-mac',
-    ]
-    locales = [l for l in locales if not l in excluded_locales]
-    locales.sort()
+    if args.locale:
+        locales = [args.locale]
+    else:
+        locales = [x for x in os.listdir(
+            l10n_clones_path) if not x.startswith('.')]
+        # Exclude locales still working on Mercurial directly
+        excluded_locales = [
+            'da', 'it', 'ja', 'ja-JP-mac',
+        ]
+        locales = [l for l in locales if not l in excluded_locales]
+        locales.sort()
 
     # Get a list of supported files in the source repository
     source_file_list = extractFileList(quarantine_path)
 
     for locale in locales:
-        print('Updating: {}'.format(locale))
+        print('Locale: {}'.format(locale))
         locale_path = os.path.join(l10n_clones_path, locale)
 
-        # Update locale repository
-        subprocess.run([
-            'hg', '-R', locale_path, 'pull', '-u'
-        ])
+        # Update locale repository, unless --noupdates was called explicitly
+        if not args.noupdates:
+            print('Updating repository...')
+            subprocess.run([
+                'hg', '-R', locale_path, 'pull', '-u'
+            ])
 
         # Create list of target files
         target_file_list = extractFileList(locale_path)
@@ -117,17 +153,20 @@ def main():
             with open(target_filename, 'wb') as f:
                 f.write(output)
 
-        # Commit changes
-        subprocess.run([
-            'hg', '-R', locale_path, 'addremove'
-        ])
-        subprocess.run([
-            'hg', '-R', locale_path, 'commit', '-m',
-            'Remove obsolete files and reformat files'
-        ])
-        subprocess.run([
-            'hg', '-R', locale_path, 'push'
-        ])
+        if args.wetrun:
+            # Commit changes
+            subprocess.run([
+                'hg', '-R', locale_path, 'addremove'
+            ])
+            subprocess.run([
+                'hg', '-R', locale_path, 'commit', '-m',
+                'Remove obsolete files and reformat files'
+            ])
+            subprocess.run([
+                'hg', '-R', locale_path, 'push'
+            ])
+        else:
+            print('(dry run')
 
 
 if __name__ == '__main__':
